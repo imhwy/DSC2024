6f77aba734f2be6072a84d7fd9d7c11d3b78a67d
PhucDanh1412 <21521924@gm.uit.edu.vn>
2024-11-02 16:57:29 +0700

    update

--- src/utils/openai_call.py
+++ src/utils/openai_call.py
@@ -2,17 +2,20 @@ import openai
 from llama_index.llms.openai import OpenAI
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-openai.api_key = OPENAI_API_KEY
 
 
-llm = OpenAI(model="gpt-4o-mini")
+llm = OpenAI(model="gpt-4o-mini", api_key=OPENAI_API_KEY)
 
 system_prompt = """From provided link, rewrite the name in vietnames with full tone mark. Only answer the refined name. Do not add additional information.
     Provided link: {link}"""
 
 
-def get_major_name_from_link(link):
+async def get_major_name_from_link(link):
     # Choose a Gemini API model.
-    response = llm.complete(system_prompt.format(link=link))
+    response = await llm.acomplete(system_prompt.format(link=link))
     return response
